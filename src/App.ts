c658eb553ada3628f8ac253879286b73e75798fe
Liya Du <liydu@microsoft.com>
2020-02-27 11:47:20 +0800

    Bump to use @airgram/web v3

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
 import { Airgram } from '@airgram/web'
+import { Auth } from './Auth' // We borrow the component only for demonstration purposes.
 import { apiHash, apiId, jsLogVerbosityLevel, logVerbosityLevel } from './config'
 
 const airgram = new Airgram({
@@ -8,40 +9,20 @@ const airgram = new Airgram({
   logVerbosityLevel
 })
 
-function syntaxHighlight (json:any) {
-  if (typeof json != 'string') {
-       json = JSON.stringify(json, undefined, 2);
-  }
-  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match:any) {
-      var cls = 'number';
-      if (/^"/.test(match)) {
-          if (/:$/.test(match)) {
-              cls = 'key';
-          } else {
-              cls = 'string';
-          }
-      } else if (/true|false/.test(match)) {
-          cls = 'boolean';
-      } else if (/null/.test(match)) {
-          cls = 'null';
-      }
-      return '<span class="' + cls + '">' + match + '</span>';
-  });
-}
+airgram.use(new Auth({
+  code: () => window.prompt('Please enter the secret code:') || '',
+  phoneNumber: () => window.prompt('Please enter your phone number:') || '',
+  password: () => window.prompt('Please enter your password:') || ''
+}))
 
 airgram.use(async (ctx, next) => {
-  const logElement: HTMLElement = document.getElementById('log')!;
   if ('request' in ctx) {
-    logElement.innerHTML += '<p> 🚀 [Airgram Request]:' + syntaxHighlight(ctx.request) + '</p>'
     console.log('🚀 [Airgram Request]:', ctx.request)
   } else if (ctx.update) {
-    logElement.innerHTML += '<p> 🚀 [Airgram Update]:' + syntaxHighlight(ctx.update) + '</p>'
     console.log('🚀 [Airgram Update]:', ctx.update)
   }
   await next()
   if ('request' in ctx) {
-    logElement.innerHTML += '<p> 🚀 [Airgram Response]:' + syntaxHighlight(ctx.response) + '</p>'
     console.log('🚀 [Airgram Response]:', ctx.request.method, ctx.response)
   }
 })
